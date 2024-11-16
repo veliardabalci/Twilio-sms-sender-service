6c4d4814dd6fe8cb2abcfe5e62b4aee599864813
Veli Arda Balcı <veliarda.balci@gmail.com>
2024-11-17 01:19:07 +0300

    main.go space

--- main.go
+++ main.go
@@ -10,19 +10,29 @@ import (
 
 func main() {
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
 	if err != nil {
 		logrus.Fatalf("Failed to open log file: %v", err)
 	}
+
 	multiWriter := io.MultiWriter(file, os.Stdout)
+
 	logrus.SetOutput(multiWriter)
+
 	logrus.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
+
 	logrus.SetLevel(logrus.InfoLevel)
+
 	server := gin.Default()
+
 	router.SetupRouter(server)
+
 	logrus.Info("Starting server on port 8000...")
+
 	err = server.Run(":8000")
+
 	if err != nil {
 		return
 	}
